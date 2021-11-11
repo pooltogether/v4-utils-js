0f7c9e969d05bf1bbfc43990e325f49b8f957e3e
Kames Geraghty <kamescg@gmail.com>
2021-11-11 10:30:41 +0000

    update(tests): add tests from the draw-calculator-js lib

--- test/computePrizeDistribution.test.ts
+++ test/computePrizeDistribution.test.ts
@@ -1,7 +1,8 @@
+import { expect } from 'chai'
 import { computePrizeDistribution } from '../src';
 
 describe('computePrizeDistribution', () => {
   it('should succeed', () => {
-
+    expect(1 + 1).equal(2);
   });
 });
