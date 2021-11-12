a88d6db9d32ec7bd451cad11c42e2d9dc0e8a34d
Kames Geraghty <kamescg@gmail.com>
2021-11-12 14:00:30 +0000

    update(types): update local types

--- src/types.ts
+++ src/types.ts
@@ -1,71 +1,72 @@
-import { Provider } from "@ethersproject/abstract-provider";
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber } from 'ethers';
 
-export type PrizeDistribution = {
-  matchCardinality: number;
-  numberOfPicks: BigNumber;
-  distributions: number[];
-  bitRangeSize: number;
-  prize: BigNumber;
-  drawStartTimestampOffset?: number;
-  drawEndTimestampOffset?: number;
-  maxPicksPerUser: number;
-};
-
-export type Draw = {
+export interface Draw {
   drawId: number;
   winningRandomNumber: BigNumber;
-  timestamp?: number;
-  beaconPeriodStartedAt?: number;
-  beaconPeriodSeconds?: number;
+  timestamp: number;
+  beaconPeriodStartedAt: number;
+  beaconPeriodSeconds: number;
 };
 
-export type Pick = {
+export interface PrizeDistribution {
+  bitRangeSize: number;
+  matchCardinality: number;
+  startTimestampOffset?: number;
+  endTimestampOffset?: number;
+  maxPicksPerUser: number;
+  expiryDuration: number;
+  numberOfPicks: BigNumber;
+  tiers: number[];
+  prize: BigNumber;
+};
+
+export interface PrizeTier {
+  bitRangeSize: number;
+  drawId: number;
+  maxPicksPerUser: number;
+  prize: number;
+  tiers: number;
+  validityDuration: number;
+}
+
+export interface Pick {
   index: number;
   hash: string;
 };
 
-export type User = {
+export interface User {
   address: string;
   normalizedBalances: BigNumber[];
   picks?: Pick[]; // optional as user may not have picks (under floor)
 };
 
-export type DrawResults = {
+export interface DrawResults {
   drawId: number;
   totalValue: BigNumber;
   prizes: PrizeAwardable[];
 };
 
-// prize that a User can receive
-export type PrizeAwardable = {
+export interface PrizeAwardable {
   amount: BigNumber;
   distributionIndex: number;
   pick: BigNumber; //populate with claim index
 };
 
-export type PickPrize = {
+export interface PickPrize {
   amount: BigNumber;
   distributionIndex: number;
 };
 
-export type Claim = {
+export interface Claim {
   userAddress: string;
   drawIds: number[];
   data: BigNumber[][];
 };
 
-export type UserDrawResult = {
+export interface UserDrawResult {
   user: User;
   drawResult: DrawResults;
 };
-
-export interface ContractAddressWithNetwork {
-  address: string;
-  name: string;
-  network: string;
-  chainId: number;
-}
 
 export interface Providers {
   [chainId: number]: Provider;
