317572fac8ab313bff2de15917130f7eb9067429
Kames Geraghty <kamescg@gmail.com>
2021-11-15 11:51:41 +0000

    add(calculatePicksFromAverageTotalSuppliesBetween.test): add function and expect statement to test

--- test/calculatePicksFromAverageTotalSuppliesBetween.test.ts
+++ test/calculatePicksFromAverageTotalSuppliesBetween.test.ts
@@ -1,5 +1,12 @@
-describe("TEMPLATE", () => {
-  it("should match the last number", () => {
-    expect(1 + 1).toEqual(2);
+import { BigNumber } from "ethers";
+import { calculatePicksFromAverageTotalSuppliesBetween } from "../src";
+
+describe("calculatePicksFromAverageTotalSuppliesBetween", () => {
+  it("should calculate the number of picks for a target ticket using total picks and total ticks supply", () => {
+    const totalPicks = 500;
+    const ticketPrimaryTotalSupply = BigNumber.from('500')
+    const otherTicketsTotalSupply = BigNumber.from('1500')
+    const picks = calculatePicksFromAverageTotalSuppliesBetween(totalPicks, ticketPrimaryTotalSupply, otherTicketsTotalSupply);
+    expect(picks).toEqual(125);
   });
 });
