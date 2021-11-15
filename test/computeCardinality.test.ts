99c536e8b3fdccff4fde3da2914cdb2eeedaa660
Kames Geraghty <kamescg@gmail.com>
2021-11-15 12:06:33 +0000

    add(computeCardinality): add function and expect statement to test

--- test/computeCardinality.test.ts
+++ test/computeCardinality.test.ts
@@ -1,5 +1,12 @@
+import { BigNumber } from 'ethers';
+import { computeCardinality } from '../src'
+
 describe("computeCardinality", () => {
-  it("should", () => {
-    expect(1 + 1).toEqual(2);
+  it("should compute cardinality for a small bitrange and total supply", () => {
+    const bitRangeSize = 2;
+    const totalSupply = BigNumber.from('450')
+    const totalSupplyDecimals = 18
+    const cardinality = computeCardinality(bitRangeSize, totalSupply, totalSupplyDecimals)
+    expect(cardinality).toEqual(2);
   });
 });
