b9897e4bb83c3f1775fa2d629c40911c7b1caa13
Kames Geraghty <kamescg@gmail.com>
2021-11-18 13:22:00 +0000

    remove(core): delete stateful functions

--- src/initialize.ts
+++
@@ -1,9 +0,0 @@
-import PoolTogetherV4 from "./PoolTogetherV4";
-import { ContractList } from "@pooltogether/contract-list-schema";
-import { Providers } from "./types";
-
-export function initialize(providers: Providers, contractList: ContractList) {
-  return new PoolTogetherV4(providers, contractList);
-}
-
-export default initialize;
--- src/utils/isContractConnectedToProvider.ts
+++
@@ -1,7 +0,0 @@
-import { Contract } from "@ethersproject/contracts";
-
-export function isContractConnectedToProvider(contract?: Contract): boolean {
-  return !!contract?.provider;
-}
-
-export default isContractConnectedToProvider;
