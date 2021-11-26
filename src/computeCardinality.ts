4b2cd5180bc3c66465fffa1bb1c303e15cad5bdf
Kames Geraghty <kamescg@gmail.com>
2021-11-26 11:53:29 +0000

    update(computeCardinality): internally use BigNumbers

--- src/computeCardinality.ts
+++ src/computeCardinality.ts
@@ -1,22 +1,32 @@
-import { BigNumber, utils } from "ethers";
+import { BigNumber } from '@ethersproject/bignumber'
+import { utils } from "ethers";
+const debug = require('debug')('pt-v4-js')
 
 export function computeCardinality(
   bitRangeSize: BigNumber,
   totalSupply: BigNumber,
   totalSupplyDecimals: BigNumber
 ): number {
+  debug("computeCardinality:ENTER")
   let numberOfPicks;
   let matchCardinality = BigNumber.from(2);
   const range = BigNumber.from(2).pow(bitRangeSize);
-
+  debug("computeCardinality:matchCardinality: ", matchCardinality.toString());
+  debug("computeCardinality:range: ", range.toString());
+  debug("computeCardinality:totalSupply: ", totalSupply.toString());
+  debug("computeCardinality:totalSupplyDecimals: ", totalSupplyDecimals.toString());
   do {
+
     numberOfPicks = utils.parseUnits(
-      `${range.pow(matchCardinality.add(1))}`,
+      `${range.pow(matchCardinality)}`,
       totalSupplyDecimals
     );
+    matchCardinality = matchCardinality.add(1);
+    debug("computeCardinality:numberOfPicks:loop: ", numberOfPicks.toString());
   } while (numberOfPicks.lt(totalSupply));
-
-  matchCardinality.sub(1);
+  debug("computeCardinality:numberOfPicks: ", numberOfPicks.toString());
+  matchCardinality = matchCardinality.sub(1);
+  debug("computeCardinality:matchCardinality: ", matchCardinality.toString());
   return matchCardinality.toNumber();
 }
 
