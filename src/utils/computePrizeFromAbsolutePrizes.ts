e86b9e56ed5cd2d4d839512b869001ccea592f0d
Brendan Asselstine <mail.asselstine@gmail.com>
2021-11-30 15:21:45 -0800

    Fixed linter errors

--- src/utils/computePrizeFromAbsolutePrizes.ts
+++ src/utils/computePrizeFromAbsolutePrizes.ts
@@ -1,18 +1,26 @@
 import { BigNumber, ethers } from "ethers";
-import { calculateNumberOfPrizesForIndex } from '../helpers/calculateNumberOfPrizesForIndex'
+import { calculateNumberOfPrizesForIndex } from "../helpers/calculateNumberOfPrizesForIndex";
 
 /**
  * Calculates the total prize payout for tiers defined absolutely.
- * 
+ *
  * @remarks
  * Given tier prizes of [2500, 800], that means there is 1 $2500 prize and 3 $800 prizes. This function
  * will add them all up.
- * 
+ *
  * @param bitRangeSize The bit range to use.  Determines the number of prizes per tier
  * @param tierPrizes The prize tiers defined in terms of absolute prize value.
  */
-export function computePrizeFromAbsolutePrizes(bitRangeSize: number, tierPrizes: BigNumber[]): BigNumber {
-    return tierPrizes.reduce((tot: BigNumber, tierPrize: BigNumber, index: number): BigNumber => {
-        return tot.add( tierPrize.mul(calculateNumberOfPrizesForIndex(bitRangeSize, index)) )
-    }, ethers.BigNumber.from('0'))
+export function computePrizeFromAbsolutePrizes(
+  bitRangeSize: number,
+  tierPrizes: BigNumber[]
+): BigNumber {
+  return tierPrizes.reduce(
+    (tot: BigNumber, tierPrize: BigNumber, index: number): BigNumber => {
+      return tot.add(
+        tierPrize.mul(calculateNumberOfPrizesForIndex(bitRangeSize, index))
+      );
+    },
+    ethers.BigNumber.from("0")
+  );
 }
--- test/utils/computeTiersFromAbsolutePrizes.test.ts
+++ test/utils/computeTiersFromAbsolutePrizes.test.ts
@@ -3,8 +3,11 @@ import { computeTiersFromAbsolutePrizes } from "../../src/utils";
 
 describe("computeTiersFromAbsolutePrizes.test", () => {
   it("should correctly sum 2 tier prizes", () => {
-    expect(computeTiersFromAbsolutePrizes(2, [BigNumber.from('2500'), BigNumber.from('800')])).toEqual(
-        [ BigNumber.from('510204081'), BigNumber.from('489795918') ]
-    );
+    expect(
+      computeTiersFromAbsolutePrizes(2, [
+        BigNumber.from("2500"),
+        BigNumber.from("800"),
+      ])
+    ).toEqual([BigNumber.from("510204081"), BigNumber.from("489795918")]);
   });
 });
