f2edc45991d0ae38ea9f23fd295d69835a5d9af2
Brendan Asselstine <mail.asselstine@gmail.com>
2021-11-30 16:23:19 -0800

    Fixed linter

--- test/helpers/calculateNumberOfPrizesForIndex.test.ts
+++ test/helpers/calculateNumberOfPrizesForIndex.test.ts
@@ -1,9 +1,9 @@
 import { calculateNumberOfPrizesForIndex } from "../../src/helpers";
 
 describe("calculateNumberOfPrizesForIndex.test", () => {
-  it('0', () => expect(calculateNumberOfPrizesForIndex(2, 0)).toEqual(1))
-  it('1', () => expect(calculateNumberOfPrizesForIndex(2, 1)).toEqual(3))
-  it('2', () => expect(calculateNumberOfPrizesForIndex(2, 2)).toEqual(12))
-  it('3', () => expect(calculateNumberOfPrizesForIndex(2, 3)).toEqual(48))
-  it('4', () => expect(calculateNumberOfPrizesForIndex(2, 4)).toEqual(192))
+  it("0", () => expect(calculateNumberOfPrizesForIndex(2, 0)).toEqual(1));
+  it("1", () => expect(calculateNumberOfPrizesForIndex(2, 1)).toEqual(3));
+  it("2", () => expect(calculateNumberOfPrizesForIndex(2, 2)).toEqual(12));
+  it("3", () => expect(calculateNumberOfPrizesForIndex(2, 3)).toEqual(48));
+  it("4", () => expect(calculateNumberOfPrizesForIndex(2, 4)).toEqual(192));
 });
