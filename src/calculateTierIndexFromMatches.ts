6de74827db62983738faef3e9b83b53528b49479
kames <kamescg@gmail.com>
2022-01-12 10:14:06 -0700

    update(calculateTierIndexFromMatches): throw error and add more tests

--- src/calculateTierIndexFromMatches.ts
+++ src/calculateTierIndexFromMatches.ts
@@ -2,6 +2,7 @@ function calculateTierIndexFromMatches(
     matchCardinality: number,
     numberOfMatches: number
 ) {
+    if(numberOfMatches > matchCardinality) throw new Error('numberOfMatches cannot be greater than matchCardinality');
     return matchCardinality - numberOfMatches;
 }
 
