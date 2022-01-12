43bc80abd3d2466c94abc3345fad51e1d043a8b7
kames <kamescg@gmail.com>
2022-01-12 09:23:59 -0700

    linting

--- src/calculateNumberOfMatches.ts
+++ src/calculateNumberOfMatches.ts
@@ -1,34 +1,31 @@
-import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
-import { findBitMatchesAtIndex } from "./utils";
+import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
+
+import { findBitMatchesAtIndex } from './utils';
 
 function calculateNumberOfMatches(
-    pickNumber: BigNumberish,
-    winningRandomNumber: BigNumberish,
-    matchCardinality: number,
-    bitRangeSize: number,
+  pickNumber: BigNumberish,
+  winningRandomNumber: BigNumberish,
+  matchCardinality: number,
+  bitRangeSize: number
 ) {
-    const _pickNumber = BigNumber.from(pickNumber);
-    const _winningRandomNumber = BigNumber.from(winningRandomNumber);
-    let numberOfMatches = 0;
-    for (
-        let matchIndex = 0;
-        matchIndex < matchCardinality;
-        matchIndex++
-      ) {
-        if (
-          !findBitMatchesAtIndex(
-            _pickNumber,
-            _winningRandomNumber,
-            matchIndex,
-            bitRangeSize
-          )
-        ) {
-          break;
-        }
-        numberOfMatches++;
-      }
+  const _pickNumber = BigNumber.from(pickNumber);
+  const _winningRandomNumber = BigNumber.from(winningRandomNumber);
+  let numberOfMatches = 0;
+  for (let matchIndex = 0; matchIndex < matchCardinality; matchIndex++) {
+    if (
+      !findBitMatchesAtIndex(
+        _pickNumber,
+        _winningRandomNumber,
+        matchIndex,
+        bitRangeSize
+      )
+    ) {
+      break;
+    }
+    numberOfMatches++;
+  }
 
-    return numberOfMatches;
+  return numberOfMatches;
 }
 
 export default calculateNumberOfMatches;
