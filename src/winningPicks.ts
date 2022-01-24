37c7c03cdc84ee505c3f5027dbb656e847596ebc
kames <kamescg@gmail.com>
2022-01-24 09:19:16 -0700

    update(winningPicks): jsdocs

--- src/winningPicks.ts
+++ src/winningPicks.ts
@@ -2,6 +2,13 @@ import computeWinningPicks from './compute/computeWinningPicks';
 import encodeWinningPicks from './encodeWinningPicks';
 import { Claim, Draw, PrizeDistribution, User } from './types';
 
+/**
+ * @description Computes a User's winning picks for multiple Draws and returns an encoded transaction.
+ * @param user 
+ * @param draws 
+ * @param prizeDistributions 
+ * @returns Claim
+ */
 function winningPicks(
     user: User,
     draws: Draw[],
