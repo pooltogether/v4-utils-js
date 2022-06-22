94ba32776209c87164b673a602130af0abfed53e
Pierrick Turelier <pierrick@turelier.com>
2022-06-22 18:44:06 -0500

    fix(compute): export computeUserPicksFromNormalizedBalance

--- src/compute/index.ts
+++ src/compute/index.ts
@@ -4,5 +4,6 @@ export { default as computePickPrize } from './computePickPrize';
 export { default as computePicksPrizes } from './computePicksPrizes';
 export { default as computePrizeAmount } from './computePrizeAmount';
 export { default as computeUserPicks } from './computeUserPicks';
+export { default as computeUserPicksFromNormalizedBalance } from './computeUserPicksFromNormalizedBalance';
 export { default as computeUserWinningPicksForRandomNumber } from './computeUserWinningPicksForRandomNumber';
 export { default as computeWinningPicks } from './computeWinningPicks';
